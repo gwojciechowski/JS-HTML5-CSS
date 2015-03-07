7b4caf41e35506d51ae2cf3b4e51a6be5970ae7d
gwojciechowski <greg.wojciechowski@gmail.com>
2015-03-07 18:21:28 +0100

    refs #lesson_2:     - wykonanie task21

---
+++ lesson_2/task21/code.js
@@ -0,0 +1,38 @@
+(function (global) {
+	var addManyValues;
+
+	if (!global.UAM) {
+		global.UAM = {};
+	}
+    
+	addManyValues = function () {
+        var sum = 0;
+        for (var i = 0; i< arguments.length; i++) {
+            sum += arguments[i];
+        }
+        return function() {
+            for (var i = 0; i < arguments.length; i++) {
+                sum += arguments[i];
+            }
+            return sum;
+        }
+	};
+
+	global.UAM.addManyValues = addManyValues;
+
+}(window));
+
+
+
+//Przykład użycia:
+//
+var addRest = UAM.addManyValues(2, 3);
+
+addRest(1, 1, 3); // 10
+addRest(3, 3); // 11
+addRest(1, 1, 1, 1, 1); //10
+
+var addOther = UAM.addManyValues(0, 10, 10);
+addOther(10); // 30
+
+
